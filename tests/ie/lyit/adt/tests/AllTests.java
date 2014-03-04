c50dd2c1f0e56e1d4bd7413b7b837a274d74d1cb
Markus Korbel <markus.korbel@lyit.ie>
2014-03-04 15:27:28 +0000

    Added tower of hanoi unit tests

--- tests/ie/lyit/adt/tests/AllTests.java
+++ tests/ie/lyit/adt/tests/AllTests.java
@@ -1,5 +1,6 @@
 package ie.lyit.adt.tests;
 
+import ie.lyit.adt.tests.algorithms.recursion.AllRecursionAlgorithmTests;
 import ie.lyit.adt.tests.algorithms.searching.AllSearchAlgorithmTests;
 import ie.lyit.adt.tests.algorithms.sorting.AllSortAlgorithmTests;
 import ie.lyit.adt.tests.datastructures.AllDataStructureTests;
@@ -17,7 +18,8 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ AllDataStructureTests.class, AllSearchAlgorithmTests.class,
-		AllSortAlgorithmTests.class, AllToolsTests.class })
+		AllSortAlgorithmTests.class, AllRecursionAlgorithmTests.class,
+		AllToolsTests.class })
 public class AllTests {
 	// Groups all sub-categories of unit tests
 }
